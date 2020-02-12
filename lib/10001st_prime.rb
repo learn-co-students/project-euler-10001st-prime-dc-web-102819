98eecdbac02c72639785aa9e24822134797ba57d
mattbechtel1 <bechma02@gmail.com>
2020-02-12 10:16:40 -0500

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,32 @@
 # Implement your procedural solution here!
+
+def is_prime?(num)
+    if num < 2
+        return false
+    end
+
+    i = 3
+
+    while i < num
+        if num % i == 0
+            return false
+        end
+        i += 1
+    end
+
+    return true
+end
+
+def prime_number_for(goal)
+    prime_nums = [2]
+    i = 3
+
+    while prime_nums.length < goal
+        if is_prime?(i)
+            prime_nums << i
+        end
+        i += 2
+    end
+
+    prime_nums[-1]
+end
